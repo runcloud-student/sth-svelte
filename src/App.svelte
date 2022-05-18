2260cf74936c84ef4769d284257ca72916d40222
runcloud <runcloud@sunth.onmicrosoft.com>
2022-05-18 21:14:16 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	export let name;
+ let a = 1;
+ let b = 2;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<label>
+ <input type=number bind:value={a} min=0 max=10>
+ <input type=range bind:value={a} min=0 max=10>
+</label>
+ 
+<label>
+ <input type=number bind:value={b} min=0 max=10>
+ <input type=range bind:value={b} min=0 max=10>
+</label>
+ 
+<p>{a} + {b} = {a + b}</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
